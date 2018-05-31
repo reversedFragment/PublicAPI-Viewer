64347d998d94cab6cd92bd5bebe9180b4dc7c675
Unknown <benadams@me.com>
2018-05-31 03:01:19 -0600

    kk

--- PublicAPI Viewer/Models/API Bookmark model.swift
+++ PublicAPI Viewer/Models/API Bookmark model.swift
@@ -1,9 +1,35 @@
+////
+////  API Bookmark model.swift
+////  PublicAPI Viewer
+////
+////  Created by Ben Adams on 5/31/18.
+////  Copyright © 2018 Ben Adams. All rights reserved.
+////
 //
-//  API Bookmark model.swift
-//  PublicAPI Viewer
+//import Foundation
 //
-//  Created by Ben Adams on 5/31/18.
-//  Copyright © 2018 Ben Adams. All rights reserved.
+//class savedAPI: Equatable, Codable {
+//    
+//    // MARK: Properties
+//    
+//    var timestamp: Date
+//    var title: String
+//    var text: String
+//    
+//    init(timestamp: Date = Date(), title: String, text: String) {
+//        
+//        self.timestamp = timestamp
+//        self.title = title
+//        self.description = description
+//    }
+//    
+//    
+//}
 //
-
-import Foundation
+//func ==(lhs: Entry, rhs: Entry) -> Bool {
+//    if lhs.timestamp != rhs.timestamp { return false }
+//    if lhs.title != rhs.title { return false }
+//    if lhs.text != rhs.text { return false }
+//    
+//    return true
+//}
